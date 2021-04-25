c368ef5d3e07aaf190f6bfd3dc0891f2654cd14a
Azazel-Givi <81898375+Azazel-Givi@users.noreply.github.com>
2021-04-25 14:41:07 +0300

    Delete NullException.java

--- NullException.java
+++
@@ -1,9 +0,0 @@
-public class NullException extends Exception{
-    String message;
-    public NullException(String var){
-        this.message = "Please, enter a " + var + " value";
-    }
-    public String getMessage(){
-        return this.message;
-    }
-}
