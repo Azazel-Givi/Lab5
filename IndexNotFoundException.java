55e6f4ac53001727ff177a94966c896b546a6280
Azazel-Givi <81898375+Azazel-Givi@users.noreply.github.com>
2021-04-25 14:40:53 +0300

    Delete IndexNotFoundException.java

--- IndexNotFoundException.java
+++
@@ -1,9 +0,0 @@
-public class IndexNotFoundException extends Exception {
-    String message;
-    public IndexNotFoundException(){
-        this.message = "Item not found";
-    }
-    public String getMessage(){
-        return this.message;
-    }
-}
