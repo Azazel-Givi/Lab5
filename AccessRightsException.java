b1dbb73675fbd7a25d4368739b7fbb2d3ce7fc23
Azazel-Givi <81898375+Azazel-Givi@users.noreply.github.com>
2021-04-25 14:39:33 +0300

    Delete AccessRightsException.java

--- AccessRightsException.java
+++
@@ -1,9 +0,0 @@
-public class AccessRightsException extends Exception{
-        String message;
-    public AccessRightsException(String str) {
-        this.message = str;
-    }
-    public String getMessage() {
-        return "Access rights exception. Unable to " + this.message + ". \n:(";
-    }
-}
