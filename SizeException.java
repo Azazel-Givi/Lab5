e972d01e5bb606fc715177db5c6e59e42a2132a2
Azazel-Givi <81898375+Azazel-Givi@users.noreply.github.com>
2021-04-26 22:32:24 +0300

    Delete SizeException.java

--- SizeException.java
+++
@@ -1,9 +0,0 @@
-public class SizeException extends Exception{
-    String message;
-    public SizeException(int value){
-        this.message = "Please enter a value less than  " + value;
-    }
-    public String getMessage(){
-        return this.message;
-    }
-}
