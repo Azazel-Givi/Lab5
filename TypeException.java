94e71b482e7f90e67a0a7a9ffd4eb152f47168b8
Azazel-Givi <81898375+Azazel-Givi@users.noreply.github.com>
2021-04-26 22:32:29 +0300

    Delete TypeException.java

--- TypeException.java
+++
@@ -1,9 +0,0 @@
-public class TypeException extends Exception{
-    String message;
-    public TypeException(String type){
-        this.message = "Please, enter a " + type + " value";
-    }
-    public String getMessage(){
-        return this.message;
-    }
-}
